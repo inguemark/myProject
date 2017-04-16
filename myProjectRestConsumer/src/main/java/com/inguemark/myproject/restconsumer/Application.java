3fb70d242bd02decbdd3a49f62579eb77d7bc42e
adrien <inguemark@gmail.com>
2017-04-16 17:46:32 +0200

    Réorganisation des projets. Ajout du projet RestConsumer.

---
+++ myProjectRestConsumer/src/main/java/com/inguemark/myproject/restconsumer/Application.java
@@ -0,0 +1,18 @@
+package com.inguemark.myproject.restconsumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        // voir https://spring.io/guides/gs/consuming-rest/
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+    }
+
+}
